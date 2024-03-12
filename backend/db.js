3d71ac4ea403150c0a0841788f0f77339cfbaea3
Yaswanth Kowtha <61146427+Yaswant-kowtha@users.noreply.github.com>
2024-03-12 23:53:05 +0000

    Backend for the project

---
+++ backend/db.js
@@ -0,0 +1,59 @@
+// backend/db.js
+const mongoose = require("mongoose");
+
+// Connect to mongoose
+mongoose.connect("mongodb+srv://ykowtha:axnRBS5SmVTwijKP@cluster0.35dpa27.mongodb.net/")
+
+// Create a schema for Users
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+// Create a model from the schema
+const User = mongoose.model('User', userSchema);
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+
+
+module.exports = {
+    User,
+    Account,
+};
+
